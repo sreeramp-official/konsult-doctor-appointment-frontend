f18de1d5e42ff40dd056d36976c16dc666f4191d
Sreeram P <psreeram3015@proton.me>
2025-03-23 18:42:11 +0530

    Updated appointment history.

--- src/AppointmentHistory.jsx
+++ src/AppointmentHistory.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth } from "./AuthContext";
-import API_URL from "../../config";
+import API_URL from "./config";
 
 const AppointmentHistory = () => {
     const { token, role } = useAuth();
